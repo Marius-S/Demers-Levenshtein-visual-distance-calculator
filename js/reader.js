0f0771f0c99f97d2ded63dcd149b483811d7216b
Marius Š <smigelskas.marius@gmail.com>
2016-11-29 20:49:27 +0200

    Some design changes

--- js/reader.js
+++ js/reader.js
@@ -5,6 +5,8 @@ function read(){
   for (var i = 0; i < first.length+2; i++) {
     array[i] = new Array(second.length);
   }
+  var div = document.getElementById('math');
+    div.innerHTML = div.innerHTML + "<br>";
   array[0][0]='X';
   array[1][0]='X';
   array[0][1]='X';
@@ -41,7 +43,14 @@ function read(){
   for (var r = 0; r < first.length+2; ++r){
     var tr = grid.appendChild(document.createElement('tr'));
     for (var c = 0; c < second.length+2; ++c){
+
       var cell = tr.appendChild(document.createElement('td'));
+      if(r == 0 || c == 0){
+        cell.className = "green";
+      }
+      else{
+        cell.className = "black";
+      }
       if (array[r][c] == null){
         if (array[r][0] == array[0][c] ){
           match = 0;
@@ -52,6 +61,24 @@ function read(){
         minimum = Math.min(array[r-1][c]+1,array[r][c-1]+1,array[r-1][c-1]+match);
         array[r][c] = minimum;
         cell.innerHTML = array[r][c];
+
+
+        div.innerHTML = div.innerHTML + "<strong>";
+        div.innerHTML = div.innerHTML + array[0][c];
+        div.innerHTML = div.innerHTML + ",";
+        div.innerHTML = div.innerHTML + array[r][0];
+
+        div.innerHTML = div.innerHTML + "</strong> = ";
+        div.innerHTML = div.innerHTML + array[r][c];
+        div.innerHTML = div.innerHTML + " (";
+          div.innerHTML = div.innerHTML + (array[r-1][c]+1);
+        div.innerHTML = div.innerHTML + ",";
+          div.innerHTML = div.innerHTML + (array[r][c-1]+1);
+          div.innerHTML = div.innerHTML + ",";
+            div.innerHTML = div.innerHTML + (array[r-1][c-1]+match);
+        div.innerHTML = div.innerHTML + ")";
+        div.innerHTML = div.innerHTML + "<br>";
+
       }else{
         cell.innerHTML = array[r][c];
       }
